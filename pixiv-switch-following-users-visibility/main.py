f37c2797cde587bfd7ce701648d171fc440a88cf
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-21 23:23:17 +0900

    feat: Added process to retrieve command-line arguments.

--- pixiv-switch-following-users-visibility/main.py
+++ pixiv-switch-following-users-visibility/main.py
@@ -1,3 +1,4 @@
+from utils.config import get_args
 from utils.logger import init_logger
 from utils.pixiv import init_api
 
@@ -12,6 +13,27 @@ class AppClass:
         # Initialize the API.
         self.api = init_api(self)
 
+        args = get_args()
+
+        # Get the type of the source restrict.
+        self.source = args["source"]
+        source = self.source
+
+        # Get the type of the target restrict.
+        self.target = args["target"]
+        target = self.target
+
+        if source == target:
+            logger.error(
+                "The same 'restrict' is specified for both Source and Target. Please specify a different 'restrict'."
+            )
+            return
+
+        logger.info(f"Direction: {source} => {target}")
+
+        # Get the mode.
+        self.mode = args["mode"]
+        self.logger.info(f"Mode: {self.mode}")
 if __name__ == "__main__":
     app = AppClass()
     app.main()
--- pixiv-switch-following-users-visibility/utils/config.py
+++ pixiv-switch-following-users-visibility/utils/config.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 
 # Load .env file and reflect environment variables.
@@ -7,3 +8,20 @@ load_dotenv()
 # Get environment variables.
 REFRESH_TOKEN = os.environ.get("REFRESH_TOKEN")
 USER_ID = os.environ.get("USER_ID")
+
+
+def get_args():
+    """
+    Get the type of target to be retrieved from the command-line argument.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", default="public")
+    parser.add_argument("--target", default="private")
+    parser.add_argument("--mode", default="copy")
+    args = parser.parse_args()
+
+    return {
+        "source": args.source,
+        "target": args.target,
+        "mode": args.mode,
+    }
